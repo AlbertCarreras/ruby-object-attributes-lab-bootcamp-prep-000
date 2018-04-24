e39d2afe56a2aed9922c8a95d41106ffeddee85b
Alberto Carreras <acarrerasc@gmail.com>
2018-04-24 22:26:23 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  def name
+    @name
+  end
+  def name=(person_name)
+    @name = dog_name
+  end
+  def breed
+    @breed
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+end
